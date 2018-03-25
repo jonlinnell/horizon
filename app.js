11bd1187c87a4eea370689db0c3fcf6e6335d854
Jon Linnell <linnell.jon@gmail.com>
2018-03-25 13:24:49 +0000

    Initial commit (basing on ricochet.)

---
+++ app.js
@@ -0,0 +1,84 @@
+/* eslint-disable no-console */
+require('dotenv').config()
+const bcrypt = require('bcryptjs')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const express = require('express')
+const fs = require('fs')
+const helmet = require('helmet')
+const http = require('http')
+const https = require('https')
+const morgan = require('morgan')
+const path = require('path')
+const rfs = require('rotating-file-stream')
+
+require('colors')
+
+const models = require('./models')
+const { User } = require('./models')
+
+const app = express()
+
+const port = process.env.API_PORT || 9000
+
+const logDir = path.join(__dirname, 'logs')
+if (!fs.existsSync(logDir)) {
+  fs.mkdirSync(logDir)
+}
+
+const accessLog = rfs('access.log', {
+  interval: '1d',
+  path: logDir
+})
+
+app.use(bodyParser.json())
+app.use(cors())
+app.use(helmet())
+app.use(cookieParser())
+app.use(morgan('combined', { stream: accessLog }))
+
+console.log(`Launching in ${process.env.NODE_ENV || 'development'} mode.`.cyan)
+
+/* Remember to filter fixed routes in the Joi schema */
+require('./routes/auth')(app)
+
+models.sequelize.sync().then(() => {
+  console.log(`[${'DB'.bold}] Connection established.`.green)
+
+  if (process.env.CREATE_DEFAULT_ADMIN) {
+    User.findOne({
+      where: { username: 'admin' },
+      attributes: { exclude: ['password'] }
+    })
+      .then((user) => {
+        if (!user) {
+          const hashedPassword = bcrypt.hashSync(process.env.DEFAULTADMIN || 'not @ password', 8)
+
+          User.create({
+            username: 'admin',
+            password: hashedPassword
+          })
+            .then(() => {
+              console.log('Default admin account doesn\'t exist. Creating it.'.yellow)
+            })
+        }
+      })
+  }
+
+  app.get('/status', (req, res) => {
+    res.status(200).json({ message: 'Hello there!' })
+  })
+
+  if (process.env.NODE_ENV === 'production') {
+    const options = {
+      cert: fs.readFileSync(process.env.SSL_CERT),
+      key: fs.readFileSync(process.env.SSL_KEY)
+    }
+    https.createServer(options, app).listen(port)
+    console.log(`[${'API'.bold}] Service (https) started on ${port}.`.green)
+  } else {
+    http.createServer(app).listen(port)
+    console.log(`[${'API'.bold}] Service (http) started on ${port}.`.green)
+  }
+})
---
+++ models/event.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+module.exports = (sequelize, DataTypes) => {
+  const Event = sequelize.define('Event', {})
+
+  return Event
+}
