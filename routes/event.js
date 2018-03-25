acc1306bbd1b09dc0d09eac67ae53cd9980cd31d
Jon Linnell <linnell.jon@gmail.com>
2018-03-25 20:12:52 +0100

    More /event verbs.

--- routes/event.js
+++ routes/event.js
@@ -1,4 +1,5 @@
 const Joi = require('joi')
+const sha256 = require('sha256')
 
 const verifyToken = require('../lib/verifyToken')
 
@@ -15,9 +16,7 @@ module.exports = (app) => {
         deleted: false
       }
     })
-      .then((events) => {
-        res.json(events)
-      })
+      .then(events => res.json(events))
   })
 
   app.post(endpoint, verifyToken, (req, res) => {
@@ -37,9 +36,39 @@ module.exports = (app) => {
     })
   })
 
-  app.get(`${endpoint}/:from/:to`, verifyToken, (req, res) => {})
-  app.get(`${endpoint}/deleted`, verifyToken, (req, res) => {})
-  app.get(`${endpoint}/:id`, verifyToken, (req, res) => {})
-  app.put(`${endpoint}/:id`, verifyToken, (req, res) => {})
-  app.delete(`${endpoint}/:id`, verifyToken, (req, res) => {})
+  // app.get(`${endpoint}/:from/:to`, verifyToken, (req, res) => {})
+  app.get(`${endpoint}/deleted`, verifyToken, (req, res) => {
+    Event.findAll({
+      where: {
+        deleted: true
+      }
+    })
+      .then(events => res.json(events))
+  })
+
+  app.get(`${endpoint}/:id`, verifyToken, (req, res) => {
+    Event.findById(req.params.id)
+      .then(event => (
+        event
+          ? res.json(event)
+          : res.status(404).send('Event not found')))
+  })
+
+  // app.put(`${endpoint}/:id`, verifyToken, (req, res) => {})
+
+  app.delete(`${endpoint}/:id`, verifyToken, (req, res) => {
+    Event.findById(req.params.id)
+      .then(event =>
+        event.update({
+          title: `${event.title}_${sha256(`${event.title}${event.createdAt}`).substr(51, 6)}`,
+          deleted: true
+        }, {
+          where: {
+            id: req.params.id
+          }
+        })
+          .then(() => res.sendStatus(200))
+          .catch(error => res.status(500).send(error)))
+      .catch(error => res.status(500).send(error))
+  })
 }
