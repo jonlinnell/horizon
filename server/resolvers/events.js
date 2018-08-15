12eecbfcf93feb8c159373835d7f957e23919640
Jon Linnell <linnell.jon@gmail.com>
2018-08-15 17:08:39 +0100

    Derestrict event and eventById queries

--- server/resolvers/events.js
+++ server/resolvers/events.js
@@ -2,28 +2,24 @@ import { randomBytes } from 'crypto'
 
 import { Event } from '../models'
 
-const events = (root, args, context) => (
-  context.user
-    ? Event.findAll({
-      where: {
-        deleted: false,
-      },
-    })
-      .then() // return the result
-      .catch(error => new Error(`Error retrieving events: ${error}`))
-    : new Error('Not authenticated.')
+const events = () => (
+  Event.findAll({
+    where: {
+      deleted: false,
+    },
+  })
+    .then() // return the result
+    .catch(error => new Error(`Error retrieving events: ${error}`))
 )
 
-export const eventById = (root, { id }, context) => (
-  context.user
-    ? Event.findById(id, {
-      where: {
-        delete: false,
-      },
-    })
-      .then() // return the result
-      .catch(error => new Error(`Error retrieving event ${id}: ${error}`))
-    : new Error('Not authenticated.')
+export const eventById = (root, { id }) => (
+  Event.findById(id, {
+    where: {
+      delete: false,
+    },
+  })
+    .then() // return the result
+    .catch(error => new Error(`Error retrieving event ${id}: ${error}`))
 )
 
 export const createNewEvent = (root, { input }, context) => (
