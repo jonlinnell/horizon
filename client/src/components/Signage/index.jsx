8040258a9d016c1b188f0b94a0a83cfb26460708
Jon Linnell <linnell.jon@gmail.com>
2018-08-19 10:48:18 +0000

    Use apollo-boost for proper Apollo Client implementation

---
+++ client/src/components/Signage/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import { allEvents } from '../../../../lib/api'
+
+import SignageEvent from '../SignageEvent'
+
+const Signage = styled.div`
+  font-family: '${props => props.theme.typeface.primary || 'Times New Roman'}';
+`
+
+const Component = () => (
+  <Signage>
+    <Query
+      query={gql`${allEvents()}`}
+      pollInterval={1000}
+    >
+      {
+        ({ loading, error, data }) => {
+          if (error) return <p>error...</p>
+          if (loading) return <p>loading...</p>
+
+          return (data.events.map(event => (
+            <div key={event.id}>
+              <SignageEvent event={event} key={event.id} />
+            </div>
+          )))
+        }
+      }
+    </Query>
+  </Signage>
+)
+
+export default Component
--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router, Route } from 'react-router'
-import { injectGlobal } from 'styled-components'
+import { injectGlobal, ThemeProvider } from 'styled-components'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+import { graphqlHost } from '../../server/config/config.json'
+import theme from './theme.json'
 
 import history from './history'
 
 import Signage from './components/Signage'
+
+const { host, port } = graphqlHost
 
 const Temp = () => (
   <div>
@@ -21,13 +28,21 @@ injectGlobal`
   }
 `
 
+const client = new ApolloClient({
+  uri: `http${process.env.NODE_ENV === 'production' ? 's' : ''}://${host}:${port}/graphql`,
+})
+
 const App = () => (
-  <Router history={history}>
-    <div>
-      <Route exact path="/signage" component={Signage} />
-      <Route exact path="/" component={Temp} />
-    </div>
-  </Router>
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <Router history={history}>
+        <div>
+          <Route exact path="/signage" component={Signage} />
+          <Route exact path="/" component={Temp} />
+        </div>
+      </Router>
+    </ThemeProvider>
+  </ApolloProvider>
 )
 
 ReactDOM.render(<App />, document.getElementById('root'))
